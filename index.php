8d2022749180644ebc562bae3c0f4c2071db777d
rizkyaz0 <rizkyadizaelani@gmail.com>
2025-11-12 10:03:46 +0700

    push awal

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Syahroni</title>
+</head>
+<body>
+    Hello Syahroni
+</body>
+</html>
